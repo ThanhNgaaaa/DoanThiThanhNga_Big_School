c69adde23a308ab4dbe628cd9c80f26c9ba5cc0e
Nga Thanh <doanthithanhnga3579@gmail.com>
2023-03-09 22:02:29 +0700

    Add project files.

---
+++ DoanThiThanhNga__Tuan3/Controllers/CourseController.cs
@@ -0,0 +1,46 @@
+﻿using DoanThiThanhNga__Tuan3.Models;
+using DoanThiThanhNga__Tuan3.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoanThiThanhNga__Tuan3.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CourseController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Course
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
---
+++ DoanThiThanhNga__Tuan3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoanThiThanhNga__Tuan3.Startup))]
+namespace DoanThiThanhNga__Tuan3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
